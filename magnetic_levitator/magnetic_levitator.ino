5059ed1708b94ecd3f935fb99a3de8777fa86fd6
tbesse2 <123403564+tbesse2@users.noreply.github.com>
2024-04-11 19:19:17 -0400

    Updated for analog reading from the hall effect sensor

--- magnetic_levitator/magnetic_levitator.ino
+++ magnetic_levitator/magnetic_levitator.ino
@@ -1,14 +1,18 @@
 
-volatile byte half_revolutions;
- unsigned int rpm;
- unsigned long timeold;
+volatile byte half_revolutions; //Variable for counting number of half revolutions detected by sensor
+ unsigned int rpm; 
+ unsigned long timeold; //Variable for storing previous time(ms)
+ double hall_effect; //Variable for storing analog reading of hall effect sensor
  void setup()
  {
    Serial.begin(115200);
    attachInterrupt(0, magnet_detect, RISING);//Initialize the intterrupt pin (Arduino digital pin 2)
+   pinMode(A6, INPUT);
+   pinMode(3, OUTPUT);
    half_revolutions = 0;
    rpm = 0;
    timeold = 0;
+   hall_effect = 0;
  }
  void loop()//Measure RPM
  {
@@ -21,6 +25,15 @@ volatile byte half_revolutions;
  }
  void magnet_detect()//This function is called whenever a magnet/interrupt is detected by the arduino
  {
-   half_revolutions++;
-   Serial.println("detect");
+    half_revolutions++;
+    Serial.println("detect");
+    hall_effect = analogRead(A6);
+    if (hall_effect >= 655 || hall_effect <= 368)
+    {
+      digitalWrite(3, LOW);
+    }
+    else
+    {
+      digitalWrite(3, HIGH);
+    }
  }
